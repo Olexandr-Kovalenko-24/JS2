bffe56ee2ad636c61bac4345f138c06ca9aac0fa
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-17 16:34:36 +0300

    removeEventListener

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 function clickHandler () {
-    alert('Achievement unlocked')
+    console.log('Click on button')
 }
 
 const btn = document.querySelector('#btnthree');
 
 btn.addEventListener('click', clickHandler)
+
+function logger() {
+    prompt('How old are you?')
+}
+
+btn.addEventListener('click', logger)
+
+btn.removeEventListener('click', logger)
