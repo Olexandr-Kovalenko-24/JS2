7f0b3c31b79cef4e44babc6834a9c2cfada6af48
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-17 16:09:52 +0300

    DOM start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function click () {
+    alert('Achievement unlocked')
+}
+
+// document.getElementsByTagName('button')
+// document.querySelectorAll()
+// document.getElementById('first-but')
+
+const collection = document.getElementsByTagName('button')
+console.log(collection)
