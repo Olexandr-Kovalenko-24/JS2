0ba7f1da396e5a69522e41f8c4907846e18c0d5f
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-20 14:41:52 +0300

    Button color task v1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-const [clouseBtn, openBtn] = document.querySelectorAll('.btn');
+// const [clouseBtn, openBtn] = document.querySelectorAll('btn');
 
-clouseBtn.addEventListener('mouseover', changeContent);
-openBtn.addEventListener('mouseover', changeContent);
+// clouseBtn.addEventListener('mouseover', changeContent);
+// openBtn.addEventListener('mouseover', changeContent);
 
-clouseBtn.addEventListener('mouseleave', changeContent);
-openBtn.addEventListener('mouseleave', changeContent);
+// clouseBtn.addEventListener('mouseleave', changeContent);
+// openBtn.addEventListener('mouseleave', changeContent);
 
-function changeContent () {
-let tmp = clouseBtn.textContent;
-clouseBtn.textContent = openBtn.textContent;
-openBtn.textContent = tmp;
+// function changeContent () {
+// let tmp = clouseBtn.textContent;
+// clouseBtn.textContent = openBtn.textContent;
+// openBtn.textContent = tmp;
+// }
+
+
+const btnCollection = document.querySelectorAll('section > button');
+
+for (const btn of btnCollection) {
+    btn.addEventListener('click', clickHandler);
 }
 
+function clickHandler() {
+    const parent = event.target.parentNode;
+    const color = event.target.textContent;
+    parent.style.backgroundColor = color;
+}
