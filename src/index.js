b3d9c65f5c0fae2a8db79237dc0e7f756fa3559d
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-10-21 16:09:56 +0300

    classList example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const h1 = document.createElement('h1');
-h1.textContent = 'Hello, DOM';
-document.body.append(h1);
+const root = document.querySelector('#root');
+const lamp = document.createElement('div');
+const toggler = document.querySelector('.toggler');
+root.append(lamp);
+// square.style.width = '100px';
+// square.style.height = '100px';
+
+
+lamp.classList.add('lamp-off', 'lamp')
+
+// lamp.id = 'testID'
+
+toggler.addEventListener('click', toggle)
+
+function toggle(){
+    lamp.classList.toggle('lamp-off');
+    lamp.classList.toggle('lamp-on')
+}
